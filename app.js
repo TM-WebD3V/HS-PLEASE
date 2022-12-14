8da4f8449b7f343b1fe1b7a690dc588a224dcf38
DevT <yasuounforgiven779@gmail.com>
2022-12-14 12:45:22 +0100

    damn

--- app.js
+++ app.js
@@ -20,24 +20,25 @@ arrowButton1.addEventListener("click", () => {
   arrowButton1.classList.toggle("is-active");
   clrBlack.classList.toggle("clr-black");
   answers.classList.toggle("visually-hidden");
+  arrowButton1.classList.toggle("not-active");
 });
 arrowButton2.addEventListener("click", () => {
   arrowButton2.classList.toggle("is-active");
   clrBlack2.classList.toggle("clr-black");
-  answers2.classList.toggle("visually-hidden");
+  answers2.classList.toggle("not-active");
 });
 arrowButton3.addEventListener("click", () => {
   arrowButton3.classList.toggle("is-active");
   clrBlack3.classList.toggle("clr-black");
-  answers3.classList.toggle("visually-hidden");
+  answers3.classList.toggle("not-active");
 });
 arrowButton4.addEventListener("click", () => {
   arrowButton4.classList.toggle("is-active");
   clrBlack4.classList.toggle("clr-black");
-  answers4.classList.toggle("visually-hidden");
+  answers4.classList.toggle("not-active");
 });
 arrowButton5.addEventListener("click", () => {
   arrowButton5.classList.toggle("is-active");
   clrBlack5.classList.toggle("clr-black");
-  answers5.classList.toggle("visually-hidden");
+  answers5.classList.toggle("not-active");
 });
